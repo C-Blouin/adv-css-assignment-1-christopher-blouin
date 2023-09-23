e95ccdf9c0872c855c5cf7917e50e91d9bab838e
C-Blouin <CBlouin.Georgian@gmail.com>
2023-09-23 18:19:01 -0400

    Added personal name to the top of the CSS and JS files

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
+/*
+
+Advanced Interface Design with CSS | Assignment 1 | Emperor Music Streaming Application
+
+Christopher Blouin
+
+*/
+
 // Assign document query selectors to variables matching the respective html element id's.
-const toggleMenu = document.getElementById('toggle-menu');
-const menuOpen = document.getElementById('open-menu');
-const menuClose = document.getElementById('close-menu');
+const toggleMenu = document.getElementById("toggle-menu");
+const menuOpen = document.getElementById("open-menu");
+const menuClose = document.getElementById("close-menu");
 
 // Assign the menu links variable to select all the links in the navigation menu.
-const menuLinks = document.querySelectorAll('#toggle-menu ul li a');
+const menuLinks = document.querySelectorAll("#toggle-menu ul li a");
 
 // Add event listeners to open and close the navigation menu.
-menuOpen.addEventListener('click', () => {
-  toggleMenu.classList.toggle('show__nav');
+menuOpen.addEventListener("click", () => {
+  toggleMenu.classList.toggle("show__nav");
 });
 
-menuClose.addEventListener('click', () => {
-  toggleMenu.classList.toggle('show__nav');
+menuClose.addEventListener("click", () => {
+  toggleMenu.classList.toggle("show__nav");
 });
 
 // This function will be called whenever a link in the menu is clicked, it will slide the menu out of the viewport as my CSS has a class named .show__nav which will slide the menu out of the viewport.
 function closeMenu() {
-  toggleMenu.classList.remove('show__nav');
+  toggleMenu.classList.remove("show__nav");
 }
 
 // Add event listeners on each link in the menu to close the menu when a link is clicked. It will invoke the closeMenu function on each link click.
 menuLinks.forEach((link) => {
-  link.addEventListener('click', closeMenu);
+  link.addEventListener("click", closeMenu);
 });
